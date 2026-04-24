d7c764de00f3fda564b148967f0e3cc069acdf2e
chengchen <a1873042943@163.com>
2026-04-24 17:39:04 +0800

    feat(compaction): create compaction module infrastructure

---
+++ src/agent/compaction/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './tiered-compaction';
+export * from './snip-strategy';
+export * from './tool-output-strategy';
+export * from './summarize-strategy';
+export * from './reactive-strategy';
+export * from './rehydrator';
